a53f7ce4aac6329c3feead599bff83ef76cc8ad3
SKToukir <toukirsk@gmail.com>
2016-11-05 00:18:21 +0600

    Initial commit

---
+++ app/src/main/java/tong/best/tong/tong/MainActivity.java
@@ -0,0 +1,122 @@
+package tong.best.tong.tong;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    String encoded_string, image_name;
+    Bitmap bitmap;
+    File file;
+    Uri file_uri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.start);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                getFileUri();
+                i.putExtra(MediaStore.EXTRA_OUTPUT,file_uri);
+                startActivityForResult(i,10);
+            }
+        });
+   }
+
+    private void getFileUri(){
+        image_name = "testing123.jpg";
+        file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+        + File.separator + image_name);
+
+        file_uri = Uri.fromFile(file);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 10 && resultCode == RESULT_OK){
+            new Encode_image().execute();
+        }
+    }
+
+    private class Encode_image extends AsyncTask<Void,Void,Void>{
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            bitmap = BitmapFactory.decodeFile(file_uri.getPath());
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG,100,stream);
+
+            byte[] array = stream.toByteArray();
+            encoded_string = Base64.encodeToString(array,0);
+            bitmap.recycle();
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            makeRequest();
+        }
+
+        private void makeRequest(){
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            StringRequest request = new StringRequest(Request.Method.POST, "http://10.42.0.1/tong/upload.php",
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+
+                            Toast.makeText(getApplicationContext(),response,Toast.LENGTH_LONG).show();
+
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            }){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    HashMap<String, String> map = new HashMap<>();
+                    map.put("encoded_string",encoded_string);
+                    map.put("image_name",image_name);
+
+                    return map;
+                }
+            };
+
+            requestQueue.add(request);
+        }
+    }
+}
